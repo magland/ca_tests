99d9a901daff19e79a089fe60aa4f5708ffac2e4
Jeremy Magland <magland@jm02.scdanet.org>
2016-07-05 12:06:40 -0400

    initial commits

--- example1/convert_raw.sh
+++ example1/convert_raw.sh
@@ -4,4 +4,4 @@
 # 2. be sure that mountainlab/mdaconvert/bin is in your path
 
 # this will create raw.mda
-mdaconvert datafile001_datafile002_66_mn_butter_500-6000_trimmin80.dat raw.mda --dtype=int16 --dims=32x*
+mdaconvert datafile001_30_mn_butter_500-6000_trimmin80.dat raw.mda --dtype=int16 --dims=32x*
