382f9b731ffbfaf7e15c4a940b8298448cab6b66
Jeremy Magland <magland@jm02.scdanet.org>
2016-07-05 11:44:37 -0400

    update instructions

--- example1/convert_raw.sh
+++ example1/convert_raw.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-#this will create raw.mda
+# 1. place the .dat file in this directory
+# 2. be sure that mountainlab/mdaconvert/bin is in your path
+
+# this will create raw.mda
 mdaconvert datafile001_datafile002_66_mn_butter_500-6000_trimmin80.dat raw.mda --dtype=int16 --dims=32x*
