3d9943d4174de6b33842321e99fa7a86fbbda09d
Jeremy Magland <jeremy.magland@gmail.com>
2016-07-08 08:28:07 -0400

    remove redundant scripts

--- example2/convert_raw.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 1. place the .dat file in this directory
-# 2. be sure that mountainlab/mdaconvert/bin is in your path
-
-# this will create raw.mda
-mdaconvert datafile001_30_mn_butter_500-6000_trimmin80.dat raw.mda --dtype=int16 --dims=32x*
--- example2/download_raw.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#Alternatively, if you have the file, just copy it over
-
-curl http://97.107.129.125/ca_tests/example1/datafile001_30_mn_butter_500-6000_trimmin80.dat > datafile001_30_mn_butter_500-6000_trimmin80.dat
